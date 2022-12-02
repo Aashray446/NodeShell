2df15be4973f70d38402ca72fa7b988120c210bd
Aashray Katiyar <aashray446@gmail.com>
2022-12-02 23:05:43 +0530

    <Feature> : [Shell intergration and file support ]

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-process.stdout.write("\nprompt > ");
-process.stdin.write(process.cwd());
+// Requirements
+import  {shellEngine} from './util/shellEngine';
+
+shellEngine();
+
+process.on('unhandledRejection', (reason, p) => {
+	console.error(reason, 'Unhandled Rejection at Promise', p);
+});
+	
+process.on('uncaughtException', err => {
+	console.error(err, 'Uncaught Exception thrown');
+	
+	process.exit(1);
+});
+
